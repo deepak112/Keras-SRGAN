7989cda237e01ab6ce3d02c3c2f9ed0cf5f4a612
Deepak Birla <birla.deepak26@gmail.com>
2019-04-19 16:17:51 +0530

    Update Utils.py

--- Utils.py
+++ Utils.py
@@ -217,7 +217,7 @@ def plot_test_generated_images_for_model(output_dir, generator, x_test_hr, x_tes
 def plot_test_generated_images(output_dir, generator, x_test_lr, figsize=(5, 5)):
     
     examples = x_test_lr.shape[0]
-    image_batch_hr = denormalize(x_test_hr)
+    image_batch_lr = denormalize(x_test_lr)
     gen_img = generator.predict(image_batch_lr)
     generated_image = denormalize(gen_img)
     
